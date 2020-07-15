76a873a381aad13df934d5ea74d6f5385fa03b93
Gyumong <rlaalsrb3559@naver.com>
2020-07-15 18:09:12 +0900

    회원가입-사용자가 현재 로그인 상태인지 확인해주는 check API 구현

--- shop-frontend/src/modules/auth.js
+++ shop-frontend/src/modules/auth.js
@@ -9,13 +9,13 @@ import *as authAPI from '../lib/api/auth';
 const CHANGE_FIELD ='auth/CHANGE_FIELD';
 const INITALIZE_FORM='auth/INITALIZE_FORM';
 
-const REGISTER= 'auth/REGISTER';
-const REGISTER_SUCCESS= 'auth/REGISTER_SUCCESS';
-const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+    'auth/REGISTER',
+);
 
-const LOGIN = 'auth/LOGIN';
-const LOGIN_SUCCESS ='auth/LOGIN_SUCCESS' ;
-const LOGIN_FAILURE= 'auth/LOGIN_FAILURE';
+const [LOGIN,LOGIN_SUCCESS,LOGIN_FAILURE]= createRequestActionTypes(
+    'auth/LOGIN',
+);
 
 
 export const changeField = createAction(
