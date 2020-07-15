1ddfbdd7ff362ed77926d86f27eff681135ecd93
Gyumong <rlaalsrb3559@naver.com>
2020-07-15 16:06:26 +0900

    리덕스 사가 모듈 관리

--- shop-frontend/src/modules/auth.js
+++ shop-frontend/src/modules/auth.js
@@ -1,8 +1,21 @@
 import {createAction, handleActions} from 'redux-actions';
 import produce from 'immer';
+import {takeLatest} from 'redux-saga/effects';
+import createRequestSaga,{
+    createRequestActionTypes,
+}from '../lib/createRequestSaga';
+import *as authAPI from '../lib/api/auth';
 
 const CHANGE_FIELD ='auth/CHANGE_FIELD';
 const INITALIZE_FORM='auth/INITALIZE_FORM';
+
+const REGISTER= 'auth/REGISTER';
+const REGISTER_SUCCESS= 'auth/REGISTER_SUCCESS';
+const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+
+const LOGIN = 'auth/LOGIN';
+const LOGIN_SUCCESS ='auth/LOGIN_SUCCESS' ;
+const LOGIN_FAILURE= 'auth/LOGIN_FAILURE';
 
 
 export const changeField = createAction(
@@ -16,6 +29,23 @@ export const changeField = createAction(
 
 export const initalizeForm = createAction(INITALIZE_FORM,form=>form); //register,login
 
+export const register = createAction(REGISTER, ({username,password})=>({
+    username,
+    password,
+}));
+export const login = createAction(LOGIN,({username,password})=>({
+    username,
+    password,
+}));
+
+// 사가 생성
+const registerSaga= createRequestSaga(REGISTER,authAPI.register);
+const loginSaga= createRequestSaga(LOGIN,authAPI.login);
+export function* authSaga(){
+    yield takeLatest(REGISTER,registerSaga);
+    yield takeLatest(LOGIN,loginSaga);
+}
+
 const initalState={
     register:{
         username:'',
@@ -26,6 +56,8 @@ const initalState={
         username:'',
         password:'',
     },
+    auth:null,
+    authError:null,
 };
 
 const auth = handleActions(
@@ -37,6 +69,29 @@ const auth = handleActions(
         [INITALIZE_FORM]: (state,{payload:form})=>({
             ...state,
             [form]:initalState[form],
+            authError:null, // 폼 전환시 회원 인증 에러 초기화
+        }),
+        // 회원가입 인증 성공 
+        [REGISTER_SUCCESS]:(state,{payload:auth})=>({
+            ...state,
+            authError:null,
+            auth,
+        }),
+        // 회원가입 인증 실패
+        [REGISTER_FAILURE]:(state,{payload:error})=>({
+            ...state,
+            authError:error,
+        }),
+        // 로그인 성공
+        [LOGIN_SUCCESS]:(state,{payload:auth})=>({
+            ...state,
+            authError:null,
+            auth,
+        }),
+        // 로그인 실패
+        [LOGIN_FAILURE]:(state,{payload:error})=>({
+            ...state,
+            authError:error,
         }),
     },
     initalState,
